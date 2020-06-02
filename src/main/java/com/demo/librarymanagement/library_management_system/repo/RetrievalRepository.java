ed845464e8b5045fe24f6f5765f7823f6cab2f07
ab95908_Naveen_Kumar <naveenkumarmano>
2020-06-02 17:25:01 +0530

    Removing unwanted imports

--- src/main/java/com/demo/librarymanagement/library_management_system/repo/RetrievalRepository.java
+++ src/main/java/com/demo/librarymanagement/library_management_system/repo/RetrievalRepository.java
@@ -4,6 +4,7 @@ import com.demo.librarymanagement.library_management_system.model.BookEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface RetrievalRepository extends JpaRepository<BookEntity, Integer> {
 
